4622b37535a52e020e6b0143fcd40834f36030bb
berryes <73071529+berryes@users.noreply.github.com>
2022-12-29 00:18:23 +0100

    finally found out how to parse json dynamically

--- src/main.rs
+++ src/main.rs
@@ -15,57 +15,33 @@ Plan 1
 
 use reqwest::Client;
 use reqwest::header::HeaderMap;
+use serde_json::{json, Value};
+use std::iter::Map;
 
-
-
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-struct  Stats{
-    followerCount: i128,
-    heart: i128,
-    videoCount: i128,
-
-}
-#[derive(Debug, Deserialize)]
-struct  Author {
-    avatarThumb:String,
-    avatarMedium:String,
-    avatarLarger:String,
-    nickname: String,
-}
-#[derive(Debug, Deserialize)]
-struct  Video {
-    author: Author,
-    authorStats:Stats,
-
-}
-#[derive(Debug, Deserialize)]
-struct Reccomendation {
-    itemList:Vec<Video>
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
    
+    
     let client:Client = reqwest::Client::new();
-    let highway: String = String::from("http://localhost:3000/data");
+    let highway: String = String::from("http://localhost:3000/tiktok");
     let mut headers = HeaderMap::new();
     headers.insert("Cookie",  String::from("asd").parse().unwrap());
 
 
-    let response: String = client.get(highway)
+    let response:String = client.get(highway)
     .headers(headers)
     .send()
     .await?
     .text()
     .await?;
 
-    println!("{}",&response.as_str());
-    let raktestu = serde_json::from_str(response.as_str())?;
 
-    println!("{:#?}", raktestu);
+    println!("{}", response);
+    let response_data: Value = serde_json::from_str(&response)?;
 
+
+    println!("{:?}", response_data["meleg"]);
 
     Ok(())
 
