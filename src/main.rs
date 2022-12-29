f2bad34d0b63bc2053edae6042c970ed1c8ac6be
berryes <73071529+berryes@users.noreply.github.com>
2022-12-29 03:39:36 +0100

    last

--- src/main.rs
+++ src/main.rs
@@ -13,35 +13,52 @@ Plan 1
 1 Visting the website and scraping from ther
 */
 
+/*
+ways to overcome the ip blocking:
+
+
+- proxies (a lot)
+- tor nodes (slow :/)
+- vpns
+
+gotta randomize request between these
+
+*/
+
 use reqwest::Client;
 use reqwest::header::HeaderMap;
-use serde_json::{json, Value};
-use std::iter::Map;
-
+use serde_json::{Value};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
    
     
     let client:Client = reqwest::Client::new();
-    let highway: String = String::from("http://localhost:3000/tiktok");
+    let highway: String = String::from("https://www.tiktok.com/api/recommend/item_list/?aid=1988&app_language=en&app_name=tiktok_web&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%29&channel=tiktok_web&cookie_enabled=true&count=9&device_id=7181251537108059653&device_platform=web_mobile&focus_state=true&from_page=fyp&history_len=2&is_fullscreen=false&is_page_visible=true&os=android&priority_region=&referer=&region=DE&screen_height=883&screen_width=412&tz_name=Europe%2FBudapest&webcast_language=en&msToken=64EFVpph0PFaRtXb68BPf7TiffeAkxUE-6uxgAfQeBBMHeu717tNpRGaMi8J8x2aP91jLzMoKYby3kcLbOTROCAn2ehVWCoSwclPmRhznT6Yr6K9XU-ErWWCfhZ6W6ClSIJM7zK8HJdFtFbbtw==&X-Bogus=DFSzKIVOxczANeMeSkV9EGO8kJ0E&_signature=_02B4Z6wo00001GjktBwAAIDBdKcKo9ZSJwxo9bCAAHmb35");
     let mut headers = HeaderMap::new();
     headers.insert("Cookie",  String::from("asd").parse().unwrap());
 
 
-    let response:String = client.get(highway)
+    
+    let response = client.get(highway)
     .headers(headers)
     .send()
-    .await?
-    .text()
     .await?;
+    
+    if {
+        println!("need to set cookies")
+    }
+    
+
+    // spent like 2 hours figuring this out btw
+    // i mean how to convert a string to json
+    println!("{:#?}",);
+
+/* 
+    let response_data: Value = serde_json::from_str(&response)?; */
 
 
-    println!("{}", response);
-    let response_data: Value = serde_json::from_str(&response)?;
-
-
-    println!("{:?}", response_data["meleg"]);
+/*     println!("{:?}", response_data["itemList"]); */
 
     Ok(())
 
